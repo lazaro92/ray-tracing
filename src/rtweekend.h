f06e4e34ccdefe8dd1d29ab69d3365828483ee7e
Albert Lázaro <lazaroalbert92@gmail.com>
2020-08-11 16:10:18 +0200

    Applied antialiasing

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -5,7 +5,7 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
-
+#include <cstdlib>
 
 // Usings
 
@@ -22,6 +22,26 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+/**
+ * @returns a random real in [0,1).
+ */
+inline double random_double() {
+	return rand() / (RAND_MAX + 1.0);
+}
+
+/**
+ * @returns a random real in [min, max).
+ */
+inline double random_double(double min, double max) {
+	return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
 }
 
 // Common Headers
