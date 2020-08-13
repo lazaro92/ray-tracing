946a368aa31dc8454ca95d7b6f7843fd08093381
Albert Lázaro <lazaroalbert92@gmail.com>
2020-08-13 16:35:50 +0200

    final version

--- src/vec3.h
+++ src/vec3.h
@@ -133,6 +133,14 @@ vec3 random_in_hemisphere(const vec3& normal) {
         return -in_unit_sphere;
 }
 
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
 /**
  * Calculates the reflected vector
  * @return the reflected vector
